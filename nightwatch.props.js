74507b3eea6f97e0b70827cae6c1aceb3462f089
Ema Fonua <efonua@MacBook-Pro-33.local>
2018-08-01 15:02:04 -0600

    fixed .gitignore

--- nightwatch.props.js
+++ nightwatch.props.js
@@ -1,5 +1,7 @@
 module.exports = {
-    resourcePath: "C:\\Users\\tsuss\\devmtn\\testing-resources\\",
+    resourcePath: "/Users/efonua/desktop/DevMtn/testingResources/",
     seleniumServer: "selenium-server-standalone-3.13.0.jar",
-    chromedriver: "chromedriver.exe"
+    chromedriver: "chromedriver" 
+
+
  }
